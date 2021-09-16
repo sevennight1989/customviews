55cff194587cecc2ffe49c64128f016093e3d9cf
pengzhang <pengzhang@pateo.com.cn>
2021-09-16 23:40:43 +0800

    1.添加detail页面2

--- AndroidFx/sunflower/src/main/java/com/zp/sunflower/PlantDetailFragment.kt
+++ AndroidFx/sunflower/src/main/java/com/zp/sunflower/PlantDetailFragment.kt
@@ -1,13 +1,16 @@
 package com.zp.sunflower
 
-import android.database.DatabaseUtils
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.zp.sunflower.data.Plant
 import com.zp.sunflower.databinding.FragmentPlantDetailBinding
 import com.zp.sunflower.viewmodels.PlantDetailViewModel
@@ -31,7 +34,7 @@ class PlantDetailFragment : Fragment() {
         ).apply {
             viewModel = plantDetailViewModel
             lifecycleOwner = viewLifecycleOwner
-            callback = Callback {plant->
+            callback = Callback { plant ->
                 plant?.let {
 
                 }
@@ -40,6 +43,29 @@ class PlantDetailFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun createShareIntent() {
+        val shareText = plantDetailViewModel.plant.value.let { plant ->
+            if (plant == null) {
+                ""
+            } else {
+                getString(R.string.share_text_plant, plant.name)
+            }
+        }
+        val shareIntent = ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(shareText)
+            .setType("text/plain")
+            .createChooserIntent()
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        startActivity(shareIntent)
+    }
+
+    private fun hideAppBarFab(fab: FloatingActionButton) {
+        val params = fab.layoutParams as CoordinatorLayout.LayoutParams
+        val behavior = params.behavior as FloatingActionButton.Behavior
+        behavior.isAutoHideEnabled = false
+        fab.hide()
     }
 
 
