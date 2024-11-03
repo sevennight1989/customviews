1832f13060b57896553c65ca13ca20872f1d5236
percy <sevennight1989@sina.com>
2024-11-04 07:27:46 +0800

    add test data

--- AndroidFx/app/src/main/java/com/android/custview/ui/RoundActivity.java
+++ AndroidFx/app/src/main/java/com/android/custview/ui/RoundActivity.java
@@ -19,15 +19,16 @@ import com.android.custview.service.IntercomTimeWindowService;
 import com.android.custview.service.PreviewWindowService;
 import com.android.custview.view.CustRoundView;
 import com.android.zp.base.BaseActivity;
+import com.android.zp.base.KLog;
 import com.ss.ugc.android.alpha_player.widget.GLTextureView;
 
 import java.io.IOException;
 
 public class RoundActivity extends BaseActivity {
     private CustRoundView custRoundView;
-//    private TextureView textureView;
+    private TextureView textureView;
 
-//    public Camera mCamera;
+    public Camera mCamera;
 
     @Override
     public int getLayout() {
@@ -37,93 +38,94 @@ public class RoundActivity extends BaseActivity {
     @Override
     public void initView() {
 //        custRoundView = findViewById(R.id.customRoundView);
-//        textureView = findViewById(R.id.textureVIew);
+        textureView = findViewById(R.id.textureVIew);
 //        new PreviewDialog(this).show();
-        startService(new Intent(this, PreviewWindowService.class));
+//        startService(new Intent(this, PreviewWindowService.class));
     }
 
     @Override
     public void initData() {
-//        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
-//            @Override
-//            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-//                try {
-//                    mCamera = Camera.open(0);
-//                    mCamera.setDisplayOrientation(90);
-//                    mCamera.setPreviewTexture(surface);
-//                    mCamera.startPreview();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-//
-//            }
-//
-//            @Override
-//            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-//                if (mCamera != null) {
-//                    mCamera.stopPreview();
-//                    mCamera.release();
-//                    mCamera = null;
-//                }
-//                return false;
-//            }
-//
-//            @Override
-//            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-//
-//            }
-//        });
-    }
-/*        textureView.setOpaque(true);
-        textureView.setClipToOutline(true);
-        textureView.setOutlineProvider(new ViewOutlineProvider() {
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
             @Override
-            public void getOutline(View view, Outline outline) {
-//                Rect rect = new Rect();
-//                view.getGlobalVisibleRect(rect);
-//                int lm = 0;
-//                int tm = 0;
-//                Rect selfRect = new Rect(lm, tm, rect.right - rect.left - lm, rect.bottom - rect.top - tm);
-//                outline.setRoundRect(selfRect, 50);
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                try {
+                    mCamera = Camera.open(0);
+                    mCamera.setDisplayOrientation(90);
+                    mCamera.setPreviewTexture(surface);
+                    mCamera.startPreview();
+                } catch (IOException e) {
+                    e.printStackTrace();
 
-                //不支持二阶贝塞尔曲线
-//                path.moveTo(0, 500);
-//                path.lineTo(0, 100);
-//                path.quadTo(0, 0, 100, 0);
-//                path.lineTo(400,0);
-//                path.quadTo(500, 0, 500, 100);
-//                path.lineTo(500,500);
-//                path.close();
-                Rect rect = new Rect();
-                view.getGlobalVisibleRect(rect);
-                Path path = new Path();
-//                RectF rectF = new RectF(rect);
-//                float[] radius = new float[8];
-//                radius[0] = 50f;
-//                radius[1] = 70f;
-//                radius[2] = 50f;
-//                radius[3] = 70f;
-//                radius[4] = 50f;
-//                radius[5] = 70f;
-//                radius[6] = 50f;
-//                radius[7] = 70f;
-//                path.addRoundRect(rectF, radius, Path.Direction.CCW);
+                }
+            }
 
-                path.moveTo(0, 500);
-                path.lineTo(0, 100);
-                path.lineTo(400, 0);
-                path.lineTo(500, 500);
-                path.close();
-                outline.setConvexPath(path);
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                    mCamera.release();
+                    mCamera = null;
+                }
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
             }
         });
+//    }
+//        textureView.setOpaque(true);
+//        textureView.setClipToOutline(true);
+//        textureView.setOutlineProvider(new ViewOutlineProvider() {
+//            @Override
+//            public void getOutline(View view, Outline outline) {
+////                Rect rect = new Rect();
+////                view.getGlobalVisibleRect(rect);
+////                int lm = 0;
+////                int tm = 0;
+////                Rect selfRect = new Rect(lm, tm, rect.right - rect.left - lm, rect.bottom - rect.top - tm);
+////                outline.setRoundRect(selfRect, 50);
+//
+//                //不支持二阶贝塞尔曲线
+////                path.moveTo(0, 500);
+////                path.lineTo(0, 100);
+////                path.quadTo(0, 0, 100, 0);
+////                path.lineTo(400,0);
+////                path.quadTo(500, 0, 500, 100);
+////                path.lineTo(500,500);
+////                path.close();
+////                Rect rect = new Rect();
+////                view.getGlobalVisibleRect(rect);
+//                Path path = new Path();
+////                RectF rectF = new RectF(rect);
+////                float[] radius = new float[8];
+////                radius[0] = 50f;
+////                radius[1] = 70f;
+////                radius[2] = 50f;
+////                radius[3] = 70f;
+////                radius[4] = 50f;
+////                radius[5] = 70f;
+////                radius[6] = 50f;
+////                radius[7] = 70f;
+////                path.addRoundRect(rectF, radius, Path.Direction.CCW);
+//
+////                path.moveTo(0, 500);
+////                path.lineTo(0, 100);
+////                path.lineTo(400, 0);
+////                path.lineTo(500, 500);
+////                path.close();
+////                outline.setConvexPath(path);
+//                KLog.logI("setConvexPath");
+//            }
+//        });
 
-    }*/
+    }
 
 //    @Override
 //    public boolean onTouchEvent(MotionEvent event) {
